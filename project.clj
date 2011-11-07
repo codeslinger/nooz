16c3e205538e61c8e3c7c944db6429d297ca447d
Kyle Burton <kyle.burton@gmail.com>
2011-11-07 17:17:26 -0500

    use .m2 for classpath; use good swank

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject nooz "0.1.0-SNAPSHOT"
             :description "Collaborative news site"
+            :local-repo-classpath true
+            :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.1"]]
             :main nooz.server)
