f8d4cef14cb51dfbe0f56e32a28a74a9e46aa0bf
Toby DiPasquale <toby@cbcg.net>
2012-07-10 00:13:31 -0400

    updated dependencies

--- project.clj
+++ project.clj
@@ -1,14 +1,13 @@
 (defproject nooz "0.1.0-SNAPSHOT"
             :description "Collaborative news site"
             ;; :local-repo-classpath true
-            :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
-            :dependencies [[org.clojure/clojure "1.2.1"]
-                           [org.clojure/clojure-contrib "1.2.0"]
+            :dev-dependencies [[swank-clojure "1.4.2"]]
+            :dependencies [[org.clojure/clojure "1.4.0"]
                            [clj-stacktrace "0.2.4"]
-                           [ring "1.0.0-RC1"]
-                           [noir "1.2.1"]
-                           [clj-time "0.3.2"]
+                           [ring "1.1.1"]
+                           [noir "1.2.2"]
+                           [clj-time "0.4.3"]
                            [org.mindrot/jbcrypt "0.3m"]
-                           [com.draines/postal "1.6.0"]
-                           [org.clojars.tavisrudd/redis-clojure "1.3.0"]]
+                           [com.draines/postal "1.8.0"]
+                           [org.clojars.tavisrudd/redis-clojure "1.3.1"]]
             :main nooz.server)
